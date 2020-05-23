b596a88795bdddaf09bb4bac06f88f5b873a6f6c
Al-Amin Talukdar <md.talukdar@bkash.com>
2020-05-23 16:33:23 +0600

    ecr created for helloworld-spring

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -9,3 +9,9 @@ module "helloworld-django" {
   name = "${local.env}-${local.project}-django"
   tags = merge(map("Name",join("-",[local.env,local.project,"status"])),map("ResourceType","ecr"),local.common_tags)
 }
+
+module "helloworld-spring" {
+  source = "../modules/ecr"
+  name = "${local.env}-${local.project}-spring"
+  tags = merge(map("Name",join("-",[local.env,local.project,"status"])),map("ResourceType","ecr"),local.common_tags)
+}
