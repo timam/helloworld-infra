5ece18a85df012319bbf5643dde3c81ef198b03a
Al-Amin Talukdar <md.talukdar@bkash.com>
2020-10-04 23:26:29 +0600

    modules updated

--- modules/autoscale/main.tf
+++ modules/autoscale/main.tf
@@ -4,18 +4,87 @@ resource "aws_autoscaling_group" "asg" {
   min_size = var.min_size
   desired_capacity = var.desired_capacity
   default_cooldown= "180"
-  health_check_grace_period = "90"
+  health_check_grace_period = "120"
   health_check_type = "ELB"
   force_delete = true
   termination_policies = ["OldestInstance", "OldestLaunchTemplate"]
   vpc_zone_identifier = var.vpc_zone_identifier
   enabled_metrics = [ "GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
-  launch_template {
-    id = var.launch_template
-    version = "$Latest"
-  }
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_id = var.launch_template
+        version = "$Latest"
+      }
 
+      dynamic "override" {
+        for_each = var.asg_mixed_instance_types
+        content {
+          instance_type = override.key
+#         weighted_capacity = override.value
+        }
+      }
+    }
+    instances_distribution {
+      on_demand_base_capacity = "0"
+      on_demand_allocation_strategy = "prioritized"
+      on_demand_percentage_above_base_capacity = var.on_demand_percentage_above_base_capacity
+      spot_instance_pools = var.spot_instance_pools
+      spot_allocation_strategy = var.spot_allocation_strategy
+    }
+  }
+  tags = concat(
+  [
+    {
+      "key"                 = "Name"
+      "value"               = var.name
+      "propagate_at_launch" = false
+    },
+  ],
+  var.tags,
+  local.tags_asg_format,
+  )
   lifecycle {
     create_before_destroy = true
   }
+}
+locals {
+  tags_asg_format = null_resource.tags_as_list_of_maps.*.triggers
+}
+
+resource "null_resource" "tags_as_list_of_maps" {
+  count = length(keys(var.tags_as_map))
+
+  triggers = {
+    "key"                 = keys(var.tags_as_map)[count.index]
+    "value"               = values(var.tags_as_map)[count.index]
+    "propagate_at_launch" = "true"
+  }
+}
+resource "aws_autoscaling_schedule" "office-time-scale-up" {
+  count = var.scale-up-policy ? 1 : 0
+  scheduled_action_name = "${var.name}-office-time-scale-up"
+  min_size = var.min_size
+  max_size = var.max_size
+  desired_capacity = var.min_size
+  recurrence = "0 3 * * SUN-THU"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+}
+resource "aws_autoscaling_schedule" "night-time-scale-down" {
+  count = var.scale-down-policy ? 1 : 0
+  scheduled_action_name = "${var.name}-night-time-scale-down"
+  min_size = 0
+  max_size = 0
+  desired_capacity = 0
+  recurrence = "0 16 * * *"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+}
+resource "aws_autoscaling_schedule" "schedule-scale-down-midnight" {
+  count = var.scale-down-policy ? 1 : 0
+  scheduled_action_name = "${var.name}-schedule-scale-down-midnight"
+  min_size = 0
+  max_size = 0
+  desired_capacity = 0
+  recurrence = "0 20 * * *"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
 }
--- modules/autoscale/variables.tf
+++ modules/autoscale/variables.tf
@@ -1,4 +1,7 @@
 variable "name" {}
+variable "asg_mixed_instance_types" {}
+variable "spot_instance_pools" {}
+variable "spot_allocation_strategy" {}
 variable "max_size" {
   type = number
 }
@@ -12,3 +15,34 @@ variable "vpc_zone_identifier" {
   type = list(string)
 }
 variable "launch_template" {}
+
+//variable "tags" {
+//  type = list(map(string))
+//}
+variable "policy_enabled" {
+  default = false
+  type = bool
+}
+
+variable "scale-up-policy" {
+  default = false
+  type = bool
+}
+variable "scale-down-policy" {
+  default = false
+  type = bool
+}
+variable "on_demand_percentage_above_base_capacity" {
+  default = 50
+  type = number
+}
+variable "tags_as_map" {
+  description = "A map of tags and values in the same format as other resources accept. This will be converted into the non-standard format that the aws_autoscaling_group requires."
+  type        = map(string)
+  default     = {}
+}
+variable "tags" {
+  description = "A list of tag blocks. Each element should have keys named key, value, and propagate_at_launch."
+  type        = list(map(string))
+  default     = []
+}
