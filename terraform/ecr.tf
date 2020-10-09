c83613f2f0e687b6cdfc41581ed7f1073f6f4395
Al-Amin Talukdar <md.talukdar@bkash.com>
2020-10-09 11:40:25 +0600

    ecr added for frontend and backend

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,10 @@
-//module "helloworld-spring" {
-//  source = "../modules/ecr"
-//  name = "${local.env}-${local.project}-spring"
-//  tags = merge(map("Name",join("-",[local.env,local.project,"status"])),map("ResourceType","ecr"),local.common_tags)
-//}
+module "helloworld-backend" {
+  source = "../modules/ecr"
+  name = "${local.env}-${local.project}-backend"
+  tags = merge(map("Name",join("-",[local.env,local.project,"backend"])),map("ResourceType","ecr"),local.common_tags)
+}
+module "helloworld-frontend" {
+  source = "../modules/ecr"
+  name = "${local.env}-${local.project}-frontend"
+  tags = merge(map("Name",join("-",[local.env,local.project,"frontend"])),map("ResourceType","ecr"),local.common_tags)
+}
