2ce4fab8ff85af946283e689eeaef865b74eb419
Al-Amin Talukdar <md.talukdar@bkash.com>
2020-11-11 01:11:01 +0600

    alternative name updated

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,7 +1,7 @@
 module "acm" {
   source = "../modules/acm"
   domain_name = "*.${aws_route53_zone.domain.name}"
-  subject_alternative_names = aws_route53_zone.domain.name
+  subject_alternative_names = [aws_route53_zone.domain.name]
   validation_record_fqdns = aws_route53_record.cert-validation.fqdn
   tags  = merge(map("Name",join("-",[local.env,local.project,"wildcard-acm"])),map("ResourceType","ACM"),local.common_tags)
 }
