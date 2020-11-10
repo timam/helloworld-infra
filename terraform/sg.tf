9f35fbca8a03e55d631d665ba2a7a8e40e597fc0
Al-Amin Talukdar <md.talukdar@bkash.com>
2020-11-11 02:14:27 +0600

    alb sg added

---
+++ terraform/sg.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "private-alb" {
+  name = "${local.env}-${local.project}-private-alb"
+  vpc_id = data.aws_vpc.vpc.id
+  ingress {
+    from_port = 443
+    protocol = "tcp"
+    to_port = 443
+    cidr_blocks = [data.aws_vpc.vpc.cidr_block]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = merge(map("Name",join("-",[local.env,local.project,"private-alb"])),map("ResourceType","sg"),local.common_tags)
+}
+
+resource "aws_security_group" "public-alb" {
+  name = "${local.env}-${local.project}-public-alb"
+  vpc_id = data.aws_vpc.vpc.id
+  ingress {
+    from_port = 443
+    protocol = "tcp"
+    to_port = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = merge(map("Name",join("-",[local.env,local.project,"public-alb"])),map("ResourceType","sg"),local.common_tags)
+}
